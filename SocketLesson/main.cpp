9a3fa06731614569885d6795773c6ad56f37a6c1
lyz <1048032143@qq.com>
2019-11-04 10:38:50 +0800

    创建客户端和服务器端

---
+++ SocketLesson/main.cpp
@@ -0,0 +1,23 @@
+#define WIN32_LEAN_AND_MEAN
+#include<Windows.h>
+#include<WinSock2.h>
+#include<iostream>
+using namespace std;
+
+//添加静态链接库
+//方法1：在代码中加入 #pragma comment(lib, "ws2_32.lib")
+//方法2：属性 - 连接器 - 输入 - "ws2_32.lib"
+//开发跨平台的c++代码时，应该使用第二种方法
+
+int main()
+{
+    WORD ver = MAKEWORD(2,2);
+    WSADATA dat;
+    //启动windows socket 网络环境
+    WSAStartup(ver, &dat);
+    
+    //关闭
+    WSACleanup();
+
+    return 0;
+}
