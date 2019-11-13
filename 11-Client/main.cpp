86d00bde2fad175bc96bed2656746942a4f85b0c
lyz <1048032143@qq.com>
2019-11-13 14:19:38 +0800

    跨平台代码，客户端启动线程

---
+++ 11-Client/main.cpp
@@ -0,0 +1,226 @@
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 
+#include<Windows.h>
+#include<WinSock2.h>
+#else
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+#endif
+#include<thread>
+#include<iostream>
+using namespace std;
+
+enum DataType
+{
+    Cmd_Login,
+    Cmd_LoginResult,
+    Cmd_Logout,
+    Cmd_LogoutResult,
+    Cmd_NewClient,
+    Cmd_Error
+};
+
+struct DataHeader
+{
+    int length;
+    DataType type;
+};
+
+struct Login :public DataHeader
+{
+    Login()
+    {
+        length = sizeof(Login);
+        type = Cmd_Login;
+    }
+    char name[32];
+    char password[32];
+};
+
+struct LoginResult :public DataHeader
+{
+    LoginResult()
+    {
+        length = sizeof(LoginResult);
+        type = Cmd_LoginResult;
+    }
+    int result;
+};
+
+struct Logout :public DataHeader
+{
+    Logout()
+    {
+        length = sizeof(Logout);
+        type = Cmd_Logout;
+    }
+    char name[32];
+};
+
+struct LogoutResult :public DataHeader
+{
+    LogoutResult()
+    {
+        length = sizeof(LogoutResult);
+        type = Cmd_LogoutResult;
+    }
+    int result;
+};
+
+struct NewClient :public DataHeader
+{
+    NewClient()
+    {
+        length = sizeof(NewClient);
+        type = Cmd_NewClient;
+        sock = 0;
+    }
+    int sock;
+};
+
+int Processor(SOCKET sock)
+{
+    DataHeader header;
+    int recvLen = recv(sock, (char*)&header, sizeof(header), 0);
+    if (recvLen <= 0)
+    {
+        return -1;
+    }
+
+    if (Cmd_LoginResult == header.type)
+    {
+        LoginResult result;
+        recv(sock, (char *)&result + sizeof(DataHeader), sizeof(LoginResult) - sizeof(DataHeader), 0);
+        if (result.result == 0)
+        {
+            cout << "Server: login success." << endl;
+        }
+        else
+        {
+            cout << "Server: login fail. " << endl;
+        }
+    }
+    else if (Cmd_LogoutResult == header.type)
+    {
+        LogoutResult result;
+        recv(sock, (char *)&result + sizeof(DataHeader), sizeof(LogoutResult) - +sizeof(DataHeader), 0);
+        if (result.result == 0)
+        {
+            cout << "Server: logout success." << endl;
+        }
+        else
+        {
+            cout << "Server: logout fail. " << endl;
+        }
+    }
+    else if (Cmd_NewClient == header.type)
+    {
+        NewClient result;
+        recv(sock, (char *)&result + sizeof(DataHeader), sizeof(NewClient) - sizeof(DataHeader), 0);
+        cout << "Server: New client join " << result.sock << endl;
+    }
+
+    return 0;
+}
+
+bool isRun = true;
+void CmdThread(SOCKET sock)
+{
+    while (true)
+    {
+        //3.输入请求命令
+        char buf[128] = {};
+        cin >> buf;
+        if (0 == strcmp(buf, "exit"))
+        {
+            isRun = false;
+            cout << "exit!" << endl;
+            break;
+        }
+        else if (0 == strcmp(buf, "login"))
+        {
+            //4.发送请求命令
+            Login login;
+            strcpy_s(login.name, "April");
+            strcpy_s(login.password, "0000");
+            send(sock, (char*)&login, sizeof(Login), 0);
+        }
+        else if (0 == strcmp(buf, "logout"))
+        {
+            Logout logout;
+            strcpy_s(logout.name, "April");
+            send(sock, (char*)&logout, sizeof(Logout), 0);
+        }
+        else
+        {
+            cout << "Cmd invalid, input again." << endl;
+        }
+    }
+}
+
+int main()
+{
+#ifdef _WIN32  
+    WORD ver = MAKEWORD(2, 2);
+    WSADATA dat;
+    WSAStartup(ver, &dat);
+#endif
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (INVALID_SOCKET == sock)
+    {
+        cout << "Create sock  failed!" << endl;
+        return -1;
+    }
+    sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(4567);
+    cout << addr.sin_port << endl;
+#ifdef _WIN32
+    addr.sin_addr.S_un.S_addr = inet_addr("1.1.1.100");
+#else
+    addr.sin_addr.s_addr = inet_addr("1.1.1.105");
+#endif   
+    if (SOCKET_ERROR == connect(sock, (sockaddr*)&addr, sizeof(addr)))
+    {
+        cout << "Connect  failed!" << endl;
+    }
+    //启动线程
+    thread t1(CmdThread, sock);
+    t1.detach();
+    while (isRun)
+    {
+        fd_set fdReads;
+        FD_ZERO(&fdReads);
+        FD_SET(sock, &fdReads);
+        timeval t = { 1.0, 0.0 };
+        int ret = select(sock + 1, &fdReads, 0, 0, &t);
+        if (ret < 0)
+        {
+            break;
+        }
+        if (FD_ISSET(sock, &fdReads))
+        {
+            FD_CLR(sock, &fdReads);
+            if (-1 == Processor(sock))
+            {
+                break;
+            }
+        }
+        //cout << "客户端空闲时间，处理其他业务" << endl;
+    }
+
+#ifdef _WIN32 
+    closesocket(sock);
+    WSACleanup();
+#else
+    close(sock);
+#endif   
+    return 0;
+}
---
+++ 11-Server/main.cpp
@@ -0,0 +1,261 @@
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 
+#include<Windows.h>
+#include<WinSock2.h>
+#else
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+#endif
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+enum DataType
+{
+    Cmd_Login,
+    Cmd_LoginResult,
+    Cmd_Logout,
+    Cmd_LogoutResult,
+    Cmd_NewClient,
+    Cmd_Error
+};
+
+struct DataHeader
+{
+    int length;
+    DataType type;
+};
+
+
+struct Login :public DataHeader
+{
+    Login()
+    {
+        length = sizeof(Login);
+        type = Cmd_Login;
+    }
+    char name[32];
+    char password[32];
+};
+
+struct LoginResult :public DataHeader
+{
+    LoginResult()
+    {
+        length = sizeof(LoginResult);
+        type = Cmd_LoginResult;
+    }
+    int result;
+};
+
+struct Logout :public DataHeader
+{
+    Logout()
+    {
+        length = sizeof(Logout);
+        type = Cmd_Logout;
+    }
+    char name[32];
+};
+
+struct LogoutResult :public DataHeader
+{
+    LogoutResult()
+    {
+        length = sizeof(LogoutResult);
+        type = Cmd_LogoutResult;
+    }
+    int result;
+};
+
+struct NewClient :public DataHeader
+{
+    NewClient()
+    {
+        length = sizeof(NewClient);
+        type = Cmd_NewClient;
+        sock = 0;
+    }
+    int sock;
+};
+
+
+vector<SOCKET> g_clients;
+
+int Processor(SOCKET clientSock)
+{
+    DataHeader header;
+    int recvLen = (int)recv(clientSock, (char*)&header, sizeof(header), 0);
+    if (recvLen <= 0)
+    {
+        cout << "Client " << clientSock << " exit" << endl;
+        return -1;
+    }
+
+    if (Cmd_Login == header.type)
+    {
+        Login login;
+        recv(clientSock, (char*)&login + sizeof(DataHeader), sizeof(Login) - sizeof(DataHeader), 0);
+        cout << "Client: " << "Type = " << login.type <<
+            ", name = " << login.name <<
+            ", password = " << login.password << endl;
+
+        LoginResult result;
+        result.result = 0;
+        send(clientSock, (const char*)&result, sizeof(LoginResult), 0);
+    }
+    else if (Cmd_Logout == header.type)
+    {
+        Logout logout;
+        recv(clientSock, (char*)&logout + sizeof(DataHeader), sizeof(Logout) - sizeof(DataHeader), 0);
+        cout << "Client: " << "Type = " << logout.type <<
+            ", name = " << logout.name << endl;
+
+        LogoutResult result;
+        result.result = 0;
+        send(clientSock, (const char*)&result, sizeof(LogoutResult), 0);
+    }
+    else
+    {
+        header.length = 0;
+        header.type = Cmd_Error;
+        send(clientSock, (const char*)&header, sizeof(DataHeader), 0);
+    }
+    return 0;
+}
+
+int main()
+{
+#ifdef _WIN32 
+    WORD ver = MAKEWORD(2, 2);
+    WSADATA dat;
+    WSAStartup(ver, &dat);
+#endif
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (INVALID_SOCKET == sock)
+    {
+        cout << "Create socket  failed!" << endl;
+        return -1;
+    }
+    cout << "Create socket success!" << endl;
+
+    sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(4567);
+#ifdef _WIN32 
+    addr.sin_addr.S_un.S_addr = INADDR_ANY;
+#else
+    addr.sin_addr.s_addr = INADDR_ANY;
+#endif
+
+    if (SOCKET_ERROR == bind(sock, (sockaddr*)&addr, sizeof(addr)))
+    {
+        cout << "Bind port failed!" << endl;
+        return -1;
+    }
+    cout << "Bind port success!" << endl;
+
+    if (SOCKET_ERROR == listen(sock, 10))
+    {
+        cout << "Listen port failed!" << endl;
+        return -1;
+    }
+    cout << "Listen port success!" << endl;
+
+    while (true)
+    {
+        fd_set fdRead;
+        fd_set fdWrite;
+        fd_set fdExcept;
+        FD_ZERO(&fdRead);
+        FD_ZERO(&fdWrite);
+        FD_ZERO(&fdExcept);
+
+        FD_SET(sock, &fdRead);
+        FD_SET(sock, &fdWrite);
+        FD_SET(sock, &fdExcept);
+
+        SOCKET maxSock = sock;
+        for (int i = 0; i < (int)g_clients.size(); i++)
+        {
+            FD_SET(g_clients[i], &fdRead);
+            if (maxSock < g_clients[i])
+            {
+                maxSock = g_clients[i];
+            }
+        }
+
+        timeval t = { 1,0 };
+        int ret = (int)select(maxSock + 1, &fdRead, &fdWrite, &fdExcept, &t);
+        if (ret < 0)
+        {
+            break;
+        }
+        if (FD_ISSET(sock, &fdRead))
+        {
+            FD_CLR(sock, &fdRead);
+            sockaddr_in clientAddr;
+            int len = sizeof(sockaddr_in);
+            SOCKET clientSock = INVALID_SOCKET;
+#ifdef _WIN32 
+            clientSock = accept(sock, (sockaddr*)&clientAddr, &len);
+#else
+            clientSock = accept(sock, (sockaddr*)&clientAddr, (socklen_t *)&len);
+#endif
+            if (INVALID_SOCKET == clientSock)
+            {
+                cout << "Invalid client socket!" << endl;
+            }
+
+            for (int i = 0; i < g_clients.size(); i++)
+            {
+                NewClient newClient;
+                newClient.sock = clientSock;
+                send(g_clients[i], (const char*)&newClient, sizeof(NewClient), 0);
+            }
+            g_clients.push_back(clientSock);
+            cout << "Client: " << inet_ntoa(clientAddr.sin_addr) << endl;
+        }
+
+        for (int i = 0; i < g_clients.size(); i++)
+        {
+            if (FD_ISSET(g_clients[i], &fdRead))
+            {
+                if (-1 == Processor(g_clients[i]))
+                {
+                    vector<SOCKET>::iterator iter = g_clients.begin() + i;
+                    if (iter != g_clients.end())
+                    {
+                        g_clients.erase(iter);
+                        i--;
+                    }
+                }
+            }
+        }
+        cout << "服务器空闲时间，处理其他业务" << endl;
+    }
+
+#ifdef _WIN32 
+    for (int i = 0; i < g_clients.size(); i++)
+    {
+        closesocket(g_clients[i]);;
+    }
+    closesocket(sock);
+    WSACleanup();
+#else
+    for (int i = 0; i < g_clients.size(); i++)
+    {
+        close(g_clients[i]);;
+    }
+    close(sock);
+#endif
+    return 0;
+}
